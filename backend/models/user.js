c2f63aea6308f9710de722143c5f606d4f13fe9a
Kelner Roberto <kelner250@hotmail.com>
2022-05-25 14:26:59 -0300

    add password on user models

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,7 @@ const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
   });
 
   return User;
