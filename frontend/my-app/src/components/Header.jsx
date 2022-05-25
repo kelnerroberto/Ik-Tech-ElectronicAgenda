6cbe04476886b9eeff979881c5b5de22ff82e260
Kelner Roberto <kelner250@hotmail.com>
2022-05-25 19:42:51 -0300

    feat: post new commitment (done)

--- frontend/my-app/src/components/Header.jsx
+++ frontend/my-app/src/components/Header.jsx
@@ -1,9 +1,24 @@
 import { Button, Flex, HStack, Input } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { POST_NEW_COMMITMENT } from "../services/commitmentService";
 
 export default function AddTaskToAgenda() {
   const [name, setName] = useState('');
   const [title, setTitle] = useState('');
+
+  const createNewCommitment = async () => {
+    try {
+      const body = {
+        name,
+        title,
+      }
+
+      await POST_NEW_COMMITMENT(body);
+
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 
   return(
     <Flex p={8}>
@@ -21,7 +36,12 @@ export default function AddTaskToAgenda() {
           value={title}
         />
       </HStack>
-      <Button ml='10px'>Add task</Button>
+      <Button
+        ml='10px'
+        onClick={() => createNewCommitment()}
+      >
+          Add task
+      </Button>
     </Flex>
   )
 }
