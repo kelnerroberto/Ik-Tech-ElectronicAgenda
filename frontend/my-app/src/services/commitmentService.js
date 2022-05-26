47a10f302bf98906e91eec2b181e0b5ac0d848f6
Kelner Roberto <kelner250@hotmail.com>
2022-05-26 12:02:10 -0300

    fix: change endpoint to get information from deployed api

--- frontend/my-app/src/services/commitmentService.js
+++ frontend/my-app/src/services/commitmentService.js
@@ -1,5 +1,5 @@
 import api from "./api";
 
-export const GET_ALL_COMMITMENTS = () => api.get('/commitments/all');
+export const GET_ALL_COMMITMENTS = () => api.get('/all');
 
-export const POST_NEW_COMMITMENT = (body) => api.post('/commitments/add', body);
+export const POST_NEW_COMMITMENT = (body) => api.post('/add', body);
