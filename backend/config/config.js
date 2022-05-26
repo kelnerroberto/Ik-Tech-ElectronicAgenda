ad7e498ed1afced854af8c4ea35261029bcac6e1
Kelner Roberto <kelner250@hotmail.com>
2022-05-26 09:05:46 -0300

    change config again to deploy

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,14 +16,7 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql'
   },
 };
--- backend/models/index.js
+++ backend/models/index.js
@@ -9,21 +9,6 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-
-if (env === 'production') {
-  // Break apart the Heroku database url and rebuild the configs we need
-  const { DATABASE_URL } = process.env;
-  const dbUrl = url.parse(DATABASE_URL);
-  const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
-  const password = dbUrl.auth.substr(dbUrl.auth.indexOf(':') + 1, dbUrl.auth.length);
-  const dbName = dbUrl.path.slice(1);
-  const host = dbUrl.hostname;
-  const { port } = dbUrl;
-  config.host = host;
-  config.port = port;
-  sequelize = new Sequelize(dbName, username, password, config);
-}
-
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
